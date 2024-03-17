8e3508f587524ccf2958f8b544871b70b434e537
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-03-17 22:27:51 +0800

    Add files via upload

---
+++ userInput/src/userInput/userInput.java
@@ -0,0 +1,24 @@
+package userInput;
+import java.util.Scanner;
+
+public class userInput {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("What is your name?");
+        String name = scanner.nextLine();
+        System.out.println("How old are you?");
+        int age = scanner.nextInt();
+
+        // Consume the newline character after nextInt()
+        scanner.nextLine();
+
+        System.out.println("What's your favorite food?");
+        String food = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like " + food);
+    }
+}
